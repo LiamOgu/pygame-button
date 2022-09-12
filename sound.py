25f543e2d05349846dc3d06c3a6f825fe45b546b
LïamA <112823931+LiamOgu@users.noreply.github.com>
2022-09-13 00:29:48 +0200

    Update sound.py

--- sound.py
+++ sound.py
@@ -5,5 +5,3 @@ class Sound:
         self.main_music = pygame.mixer_music.load("ZeldaMusic.mp3")
         self.main_music_play = pygame.mixer.music.play(loops=-1)
         self.main_music_volume = pygame.mixer.music.set_volume(0.25)
-
-        self.sound_volume()
